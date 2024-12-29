d959fce166cb5f11b3b060aec52fbc2d9ab19268
jonnie254 <johnkamanu254@proton.me>
2024-12-29 12:41:00 +0300

    add discovery server, customer service

---
+++ services/customer/src/main/java/com/jonnie/ecommerce/controllers/CustomerController.java
@@ -0,0 +1,13 @@
+package com.jonnie.ecommerce.controllers;
+
+import com.jonnie.ecommerce.services.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/customers")
+@RequiredArgsConstructor
+public class CustomerController {
+    private final CustomerService customerService;
+}
